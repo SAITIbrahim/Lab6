2d50396ef6a31c940981eb02a3afd0d14852cd37
SAITIbrahim <98134542+SAITIbrahim@users.noreply.github.com>
2022-03-11 23:12:59 -0700

    Initial commit

---
+++ src/main/java/services/RoleService.java
@@ -0,0 +1,22 @@
+package services;
+
+import dataaccess.RoleDB;
+import java.util.List;
+import sait.ca.lab6.models.Role;
+
+
+
+/**
+ *
+ * @author hughm
+ */
+public class RoleService {
+    
+    private RoleDB roleDB = new RoleDB();
+    
+    public List<Role> getAll() throws Exception {
+        List<Role> roles = this.roleDB.getAll();
+        return roles;
+    }
+    
+}
